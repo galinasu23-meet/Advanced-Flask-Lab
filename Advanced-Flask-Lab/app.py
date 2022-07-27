c0d2f6b43d29afe55e8209ea92a41dede6612a05
galinasu23-meet <109904723+galinasu23-meet@users.noreply.github.com>
2022-07-27 10:17:10 +0300

    changes
    yesterday i didn't understand how to do task no,3 because it showed me only the pictures and not the captions but then Roni helped me and its working right now

--- Advanced-Flask-Lab/app.py
+++ Advanced-Flask-Lab/app.py
@@ -14,7 +14,7 @@ image_link = "https://scontent.ftlv21-1.fna.fbcdn.net/v/t31.18172-1/1502644_1015
 user_bio = "galina's page :O "
 
 posts = {
-    "https://scontent.ftlv1-1.fna.fbcdn.net/v/t39.30808-6/243021542_10158462822407507_4186737446569136175_n.jpg?stp=cp0_dst-jpg_e15_fr_q65&_nc_cat=101&ccb=1-7&_nc_sid=ed5ff1&_nc_ohc=nHa7N_8RkP0AX92LvUb&_nc_ht=scontent.ftlv1-1.fna&oh=00_AT8kLSHR6V-bvFjpXVVzHXWXviYhBJgkgHANsGlmTojibg&oe=62E08013": "2021 cohort's Y3 Accelerator!",
+    "https://i.insider.com/61d1c0e2aa741500193b2d18?width=1136&format=jpeg": "2021 cohort's Y3 Accelerator!",
     "https://media-exp1.licdn.com/dms/image/C4D1BAQFTbpgMk3KTSg/company-background_10000/0/1614595305396?e=1659178800&v=beta&t=OiSIvxsPJiJkArJIzBCKVF0_-yEta9gv1qLVnViU8bo": "MEET graduation!",
     "https://pbs.twimg.com/media/FPvsO6xVkAEcrBm?format=jpg&name=900x900": "#Throwback to one of our favorite #MEETsummer events: #BowlingNight!",
     "https://pbs.twimg.com/media/FI_UkcnVIAAUvWN?format=jpg&name=medium": "2020 cohort in their Y1 summer!"}
